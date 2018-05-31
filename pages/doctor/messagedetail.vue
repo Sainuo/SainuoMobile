4cb348ae45d09926111c3c6d1344b2a89dcc5ba4
MinuteReversal <114233763@qq.com>
2018-05-31 10:05:47 +0800

    messagedetail

---
+++ pages/doctor/messagedetail.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <van-cell-group>
+            <van-field
+                v-model="title"
+                label="留言标题"
+                icon="clear"
+                placeholder="请输入用户名"
+                required
+                disabled
+            />
+            <van-field
+                v-model="content"
+                label="留言内容"
+                icon="clear"
+                placeholder="请输入用户名"
+                required
+                disabled
+            />
+            <van-field
+                v-model="message"
+                label="回复内容"
+                type="textarea"
+                placeholder="请输入留言"
+                rows="1"
+                autosize
+            />
+        </van-cell-group>
+        <van-button @click="onSubmit" type="primary">保存回复</van-button>
+    </div>
+</template>
+<script>
+import axios from "axios"
+export default {
+    data(){
+        return {
+            title:"",
+            content:"",
+            message:""
+        };
+    },
+    methods:{
+        onSubmit(){
+
+        }
+    }
+}
+</script>
